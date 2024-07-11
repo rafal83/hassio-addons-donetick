7b050bc1f0262df63d34b611a797bcf65292c079
Mohamad Tarbin <mhed.t91@gmail.com>
2024-07-10 21:25:55 -0400

    chore: Bump Donetick Addon version to 0.0.25 and update config.yaml

--- donetick/entrypoint.sh
+++ donetick/entrypoint.sh
@@ -1,35 +1,27 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Start the donetick service with environment setup and process management
-# ==============================================================================
+#!/bin/sh
 
-# Set environment variables
+# Envirment variables:
 # export VITE_APP_API_URL=http://host.docker.internal:2021
 
+# Start donetick backend and save PID 
+./donetick-core &
+PID1=$!
 
-# Read telegram token from addon config
-DONETICK_TELEGRAM_TOKEN=$(bashio::config 'telegram_token')
-
-# Export telegram token
-export DONETICK_TELEGRAM_TOKEN
-
-# Start donetick backend and save PID
-# ./donetick-core &
-# PID1=$!
-
-# Start nginx in the foreground as the second process
+# Start the second process in the background(ngnix):
 nginx -g 'daemon off;' &
 PID2=$!
 
+
+
 cleanup() {
     echo "Terminating processes..."
-    kill $PID2
+    kill $PID1 $PID2
 }
 
 # Trap SIGINT & SIGTERM to clean up before exiting
 trap cleanup SIGINT SIGTERM
 
 # Wait for both processes to exit
-wait  $PID2
+wait $PID1 $PID2 
 
 echo "Both processes have completed."
